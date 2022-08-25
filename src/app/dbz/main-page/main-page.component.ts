aa9a0be98c92d1fceb18120a2e5ef3b8716da5ec
LuisBalan <jlbalcz@gmail.com>
2022-08-25 16:08:30 -0500

    last changes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Custom {
+  name: string,
+  strength: number
+}
 
 @Component({
   selector: 'app-main-page',
@@ -6,5 +13,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainPageComponent {
+
+  newCustom: Custom = {
+    name: 'Trucks',
+    strength: 14000
+  };
+
+  error: boolean = true;
+  todos: string[] = ['sweep', 'drink', 'food'];
+  errorMessage: string = "An error has occurred";
+
+  add(){
+    console.log(this.newCustom);
+  };
+
+  changeName(event: any){
+    console.log(event.target.value);
+  };
 
 };
