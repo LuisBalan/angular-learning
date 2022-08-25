6b6c00391668cc5083b182bdb1c910b38eaf3703
LuisBalan <jlbalcz@gmail.com>
2022-08-25 16:10:02 -0500

    add changes

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Custom {
+  name: string;
+  strength: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 
 export class MainPageComponent {
+
+  newCustom: Custom = {
+    name: "Trucks",
+    strength: 14000
+  }
 
 };
